a27c5cd46c44410de099976d9ba83e56a6bdeeb8
zainalwan <zainalwan4@gmail.com>
2020-12-27 21:57:32 +0700

    Create users table, its migration and seeder

---
+++ database/migrations/2020_12_27_213914_create_users_table.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| LICENSE
+|--------------------------------------------------------------------------
+| Code that written below is belong to Zain Alwan Wima Irfani. You may
+| not use, share, modify, and study without the author's permission
+| (zainalwan4@gmail.com).
+*/
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('user_name', 255);
+            $table->string('password', 255);
+            $table->integer('active');
+            
+            $table->string('account_activation_token', 255)->nullable();
+            $table->string('password_reset_token', 255)->nullable();
+            
+            $table->integer('try_count')->nullable();
+            $table->timestamp('last_try')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| LICENSE
+|--------------------------------------------------------------------------
+| Code that written below is belong to Zain Alwan Wima Irfani. You may
+| not use, share, modify, and study without the author's permission
+| (zainalwan4@gmail.com).
+*/
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Zain Alwan Wima Irfani',
+            'user_name' => '__zainalwan',
+            'password' => Hash::make('mangga90Kering?'),
+            'active' => 1,
+        ]);
+    }
+}
