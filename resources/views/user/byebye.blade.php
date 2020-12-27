08b7d7ed925c44622cba6fa5355a83bf75da6727
zainalwan <zainalwan4@gmail.com>
2020-12-27 10:20:37 +0700

    Create byebye view

---
+++ resources/views/user/byebye.blade.php
@@ -0,0 +1,19 @@
+@php
+/*
+|--------------------------------------------------------------------------
+| LICENSE
+|--------------------------------------------------------------------------
+| Code that written below is belong to Zain Alwan Wima Irfani. You may
+| not use, share, modify, and study without the author's permission
+| (zainalwan4@gmail.com).
+*/
+@endphp
+
+@extends('layout.app')
+
+@section('content')
+    <p>
+        Hi, User. Your account was successfully deleted. Thank you for
+        choosing our application.
+    </p>
+@endsection
