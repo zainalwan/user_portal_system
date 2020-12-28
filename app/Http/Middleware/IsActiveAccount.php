f9c9adf608c292797f73b68d6fba48f2ac9b07a1
zainalwan <zainalwan4@gmail.com>
2020-12-28 13:20:54 +0700

    Create IsActiveAccount middleware

---
+++ app/Http/Middleware/IsActiveAccount.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| LICENSE
+|--------------------------------------------------------------------------
+| Code that written below is belong to Zain Alwan Wima Irfani. You may
+| not use, share, modify, and study without the author's permission
+| (zainalwan4@gmail.com).
+*/
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class IsActiveAccount
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $user_id = $request->session()->get('_ticket')['id'];
+        $user = DB::table('users')
+              ->where('id', $user_id)
+              ->first();
+
+        if($user->active == 1)
+        {
+            return $next($request);
+        }
+
+        if($user->active == -1)
+        {
+            return redirect('/warning');
+        }
+
+        if($user->active == 0)
+        {
+            return redirect('/verify');
+        }
+    }
+}
