4d26f8ad934282f13dcada41d723b23255387525
zainalwan <zainalwan4@gmail.com>
2020-12-28 21:06:27 +0700

    Create AuthenticateUserRequest

---
+++ app/Http/Requests/AuthenticateUserRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| LICENSE
+|--------------------------------------------------------------------------
+| Code that written below is belong to Zain Alwan Wima Irfani. You may
+| not use, share, modify, and study without the author's permission
+| (zainalwan4@gmail.com).
+*/
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+use App\Rules\RegisteredEmail;
+use App\Rules\CorrectPassword;
+
+class AuthenticateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'email' => [new RegisteredEmail],
+            'password' => [new CorrectPassword]
+        ];
+    }
+}
