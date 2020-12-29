cc87e60bce84e28cb66e97485529322fcbafe8d6
zainalwan <zainalwan4@gmail.com>
2020-12-29 12:56:53 +0700

    Create CorrectCurrentPassword rule

---
+++ app/Rules/CorrectCurrentPassword.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| LICENSE
+|--------------------------------------------------------------------------
+| Code that written below is belong to Zain Alwan Wima Irfani. You may
+| not use, share, modify, and study without the author's permission
+| (zainalwan4@gmail.com).
+*/
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Facades\DB;
+
+class CorrectCurrentPassword implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $user_id = session('_ticket')['id'];
+        $user = DB::table('users')
+              ->where('id', $user_id);
+
+        return Hash::check($value, $user->password);
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'Please enter your valid current password.';
+    }
+}
