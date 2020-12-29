a4c26d1335bf4111968f26d3890c60cb0d8a1dce
zainalwan <zainalwan4@gmail.com>
2020-12-29 20:41:19 +0700

    Create CreateNewPasswordRequest

---
+++ app/Http/Requests/CreateNewPasswordRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| LICENSE
+|--------------------------------------------------------------------------
+| Code that written below is belong to Zain Alwan Wima Irfani. You may
+| not use, share, modify, and study without the author's permission
+| (zainalwan4@gmail.com).
+*/
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateNewPasswordRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'new_password' => ['bail', 'regex:/[a-z]/', 'regex:/[A-Z]/', 'regex:/[0-9]/', 'min:8']
+        ];
+    }
+
+    /**
+     * Get error messages
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'new_password.regex' => 'New password should contain lowecase, uppercase, and number.'
+        ];
+    }
+}
